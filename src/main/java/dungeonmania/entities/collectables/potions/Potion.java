4cd28521ca09517f8ff2bddbc59c02a86c968ec0
Ethan <z5369412@unsw.edu.au>
2022-11-07 01:22:18 +1100

    cancel Potion implements BattleItem

--- src/main/java/dungeonmania/entities/collectables/potions/Potion.java
+++ src/main/java/dungeonmania/entities/collectables/potions/Potion.java
@@ -1,14 +1,12 @@
 package dungeonmania.entities.collectables.potions;
 
-import dungeonmania.Game;
 import dungeonmania.battles.BattleStatistics;
-import dungeonmania.entities.BattleItem;
 import dungeonmania.entities.Player;
 import dungeonmania.entities.collectables.Collectable;
 import dungeonmania.entities.inventory.InventoryItem;
 import dungeonmania.util.Position;
 
-public abstract class Potion extends Collectable implements InventoryItem, BattleItem {
+public abstract class Potion extends Collectable implements InventoryItem {
     private int duration;
 
     public Potion(Position position, int duration) {
@@ -16,24 +14,11 @@ public abstract class Potion extends Collectable implements InventoryItem, Battl
         this.duration = duration;
     }
 
-    @Override
-    public void use(Game game) {
-        return;
-    }
-
     public int getDuration() {
         return duration;
     }
 
-    @Override
-    public BattleStatistics applyBuff(BattleStatistics origin) {
-        return origin;
-    }
-
-    @Override
-    public int getDurability() {
-        return 1;
-    }
+    public abstract BattleStatistics applyBuff(BattleStatistics origin);
 
     public abstract void transitionState(Player player);
 }
