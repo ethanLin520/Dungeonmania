5e8ec9ee0cbae7e9813a7306651e26c349e98dd8
Ethan <z5369412@unsw.edu.au>
2022-11-10 00:45:57 +1100

    implement tests for stuck

--- src/test/java/dungeonmania/mvp/SwampTest.java
+++ src/test/java/dungeonmania/mvp/SwampTest.java
@@ -36,4 +36,56 @@ public class SwampTest {
         assertEquals(0, res.getBattles().size());
     }
 
+    @Test
+    @Tag("18-2")
+    @DisplayName("Test swamp stuck for correct duration.")
+    public void swampStuckDuration() {
+        /*
+         *  Move Factor = 5
+         * P (1,1)                      S (10,1)    M (11,1)
+         */
+        DungeonManiaController dmc;
+        dmc = new DungeonManiaController();
+        DungeonResponse res = dmc.newGame("d_SwampTestDuration", "c_SwampTestMovement");
+
+        assertEquals(new Position(11, 1), MercenaryTest.getMercPos(res));
+
+        res = dmc.tick(Direction.RIGHT);    // M (10,1) moves onto the swamp and get stuck
+        assertEquals(new Position(10, 1), MercenaryTest.getMercPos(res));
+        res = dmc.tick(Direction.RIGHT);    // stuck
+        res = dmc.tick(Direction.RIGHT);    // stuck
+        res = dmc.tick(Direction.RIGHT);    // stuck
+        res = dmc.tick(Direction.RIGHT);    // stuck
+        res = dmc.tick(Direction.RIGHT);    // stuck
+        res = dmc.tick(Direction.RIGHT);    // M moves off the swamp
+
+        assertEquals(new Position(9, 1), MercenaryTest.getMercPos(res));
+        assertEquals(0, res.getBattles().size());
+    }
+
+    @Test
+    @Tag("18-3")
+    @DisplayName("Test swamp moves off in the right direction.")
+    public void swampMoveOff() {
+        /*
+         *  Move Factor = 5
+         * P (1,1)                      S (7,1)     M (8,1)
+         */
+        DungeonManiaController dmc;
+        dmc = new DungeonManiaController();
+        DungeonResponse res = dmc.newGame("d_SwampTestMoveOff", "c_SwampTestMovement");
+
+        assertEquals(new Position(8, 1), MercenaryTest.getMercPos(res));
+
+        res = dmc.tick(Direction.RIGHT);    // M (8,1) moves onto the swamp and get stuck
+        assertEquals(new Position(7, 1), MercenaryTest.getMercPos(res));
+        res = dmc.tick(Direction.RIGHT);    // stuck
+        res = dmc.tick(Direction.RIGHT);    // stuck
+        res = dmc.tick(Direction.RIGHT);    // stuck
+        res = dmc.tick(Direction.RIGHT);    // stuck
+        res = dmc.tick(Direction.UP);    // stuck
+        res = dmc.tick(Direction.RIGHT);    // M moves off the swamp
+
+        assertNotEquals(0, res.getBattles().size());
+    }
 }
